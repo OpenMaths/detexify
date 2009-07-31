566eb32c1a5f4478a30c71404b899344bef43476
Daniel Kirsch <danishkirel@gmail.com>
2009-07-31 08:30:36 +0200

    capthin

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,15 +18,16 @@ set :application, "detexify"
 # load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
 # load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
 
-set :deploy_to, "/home/kirel/#{application}"
+set :user, 'kirel'
+
+set :deploy_to, "/home/#{user}/#{application}"
  
 default_run_options[:pty] = true
 set :scm, :git
 set :repository,  "git@github.com:kirel/detexify.git"
-set :branch, "master"
+set :branch, "capthin"
 set :deploy_via, :remote_cache
  
-set :user, 'kirel'
 set :ssh_options, { :forward_agent => true }
 set :port, 7822
 set :use_sudo, false
@@ -46,11 +47,12 @@ namespace :deploy do
   task :cold do
      deploy.update
      deploy.start
+     thin.config
   end
 end
 
 namespace :thin do  
-  %w(start stop restart).each do |action| 
+  %w(start stop restart config).each do |action| 
   desc "#{action} the app's Thin Cluster"  
     task action.to_sym, :roles => :app do  
       run "thin #{action} -c #{deploy_to}/current -C #{deploy_to}/current/config/thin.yml" 
