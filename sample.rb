13e6f4d70d30c21c3c29c2b9012bed24b92e15e0
Daniel Kirsch <danishkirel@gmail.com>
2009-08-08 16:31:54 +0200

    new backend for sample

--- sample.rb
+++ sample.rb
@@ -1,37 +1,9 @@
+require 'mongo'
+require 'digest/sha1'
+
 module Detexify
-    
-  class Sample < CouchRest::ExtendedDocument
-    property :feature_vector
-    property :strokes
-    property :symbol_id
-    
-    view_by :symbol_id
-    
-    #timestamps!
-    
-    def symbol
-      Latex::Symbol[symbol_id]
-    end
-    
-  end
   
-  # These will stay in memory all the time
-  class MiniSample
-    
-    attr_reader :feature_vector, :symbol_id
-    
-    def initialize sample
-      @feature_vector = sample.feature_vector
-      @symbol_id = sample.symbol_id.to_sym # to_sym so only one instance of the string exists
-    end
-    
-    def == other
-      @symbol_id == other.symbol_id && @feature_vector == other.feature_vector
-    end
-    
-  end
-  
-  class MiniSampleContainer
+  class CappedContainer
     
     include Enumerable
     
@@ -40,13 +12,27 @@ module Detexify
       @hash = Hash.new { |h,v| h[v] = [] }
     end
     
+    def clear
+      @hash = Hash.new { |h,v| h[v] = [] }
+    end
+    
+    def limit= newlimit
+      if newlimit < @limit
+        @hash.each do |_, a|
+          while a.size > newlimit
+            a.shift
+          end
+        end
+      end
+      @limit = newlimit
+    end
+    
     def << sample
-      sample =  MiniSample.new(sample) unless sample.is_a? MiniSample
       a = @hash[sample.symbol_id]
       a << sample
       a.shift if a.size > @limit
       self
-    end  
+    end
       
     def each &block
       @hash.each do |id, a|
@@ -56,8 +42,119 @@ module Detexify
       end
     end
     
-    def for_id id
-      @hash[id]
+  end
+  
+  class Sample
+    
+    @@sample_limit = 100
+    @@db = XGen::Mongo::Driver::Mongo.new(ENV['mongo'] || 'localhost').db("detexify")
+    # there will also be capped collections for each id
+    
+    @@memcache = CappedContainer.new @@sample_limit
+    
+    @@progress = Class.new do
+      def + num
+        @num ||= 0
+        @num += num
+        self
+      end
+      
+      def inc
+        self + 1
+      end
+      
+      def to_i
+        [100*@num/Latex::Symbol::List.size, 100].min
+      end
+    end.new
+    
+    def self.progress
+      @@progress.to_i
+    end
+    
+    def self.db=thedb
+      @@db = thedb
+    end
+    
+    def self.sample_limit=limit
+      @@sample_limit = limit
+      @@memcache.limit = limit
+    end
+
+    def initialize h
+      # :feature_vector, :strokes, :symbol_id
+      @sample = h
+    end
+    
+    [:feature_vector, :strokes, :symbol_id].each do |m|
+      define_method m do
+        @sample[m.to_s]
+      end
+
+      define_method :"#{m}=" do |val|
+        @sample[m.to_s] = val
+      end
+    end
+    
+    def save
+      @sample['_id'] = @@db.collection('samples').save(@sample)
+      # create db cache as capped collection
+      sid = @sample['symbol_id'].to_s
+      hid = sid #Digest::SHA1.hexdigest(sid)
+      unless @@db.collection_names.include? hid
+        @@db.create_collection hid, :capped => true, :max => @@sample_limit
+      end
+      @@db.collection(hid).save(@sample.dup.delete_if { |k,_| k != 'feature_vector' })
+      # and push into memcache
+      @@memcache << self.class.new(@sample.dup).strip!
+      # but return sample from collection('samples')
+      @sample # this is the hash from the db - not self
+    end
+    
+    def self.each_stripped &block
+      @@memcache.each(&block)
+    end
+    
+    def self.load(join = false)
+      Thread.abort_on_exception = true
+      @load_thread = Thread.new do
+        Latex::Symbol::List.each do |symbol|
+          sid = symbol.id.to_s
+          hid = sid #Digest::SHA1.hexdigest(sid)
+          if @@db.collection_names.include? hid # yeah! cached samples in db
+            @@db.collection(hid).find().each do |h|
+              @@memcache << self.new(h.update('symbol_id' => sid)).strip!
+            end
+          else # no db cache - create it
+            @@db.create_collection hid, :capped => true, :max => @@sample_limit
+            @@db.collection('samples').find('symbol_id' => sid).sort_by { rand }[0..(@@sample_limit-1)].each do |h|
+              # save to db cache and push to memcache
+              stripped = self.new(h).strip!
+              @@db.collection(hid).save(h.delete_if { |k,_| k != 'feature_vector' })
+              @@memcache << stripped
+            end # FIXME symbols/strings
+          end
+          @@progress.inc
+        end # Latex::Symbol::List.each
+      end
+      @load_thread.join if join
+    end
+    
+    def self.unload
+      @@memcache.clear
+    end
+    
+    def symbol
+      Latex::Symbol[symbol_id.to_sym]
+    end
+
+    def strip!
+      @sample.delete_if { |k,_| !%w(symbol_id feature_vector).include? k }
+      self.extend(Module.new do
+        def save
+          raise "Don't save a stripped sample!"
+        end
+      end)
     end
     
   end
--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -4,46 +4,131 @@ require 'sample'
 
 describe Detexify::Sample do
   before do
-    @db = CouchRest.database! TESTCOUCH
+    @mongo = XGen::Mongo::Driver::Mongo.new('localhost')
+    @mongo.drop_database('detexify-test')
+    @db = @mongo.db('detexify-test')
+    @limit = 2
+    Detexify::Sample.db = @db
+    Detexify::Sample.sample_limit = @limit
     @symbol = Latex::Symbol::List.first
     @strokes = [[{'x'=>1,'y'=>1}]]
+    @fvector = [1]
   end
   
   after do
-    @db.delete!
+    @mongo.drop_database('detexify-test')
   end
   
-  it "can be created and saved" do
-    sample = Detexify::Sample.on(@db).new :strokes => @strokes, :feature_vector => [], :symbol_id => @symbol.id
-    lambda { sample.create! }.should_not raise_error     
+  it "can be saved" do
+    sample = Detexify::Sample.new('strokes' => @strokes, 'feature_vector' => [1], 'symbol_id' => @symbol.id)
+    lambda { sample.save }.should_not raise_error     
   end
+  
+  describe "a stripped sample" do
+    
+    before(:each) do
+      @sample = Detexify::Sample.new('strokes' => @strokes, 'feature_vector' => [1], 'symbol_id' => @symbol.id).strip!
+    end
+    
+    it "can not be saved" do
+      lambda { @sample.save }.should raise_error
+    end
+    
+  end
+  
+  describe "a saved sample" do
+    
+    before(:each) do
+      @sample = Detexify::Sample.new('strokes' => @strokes, 'feature_vector' => [1], 'symbol_id' => @symbol.id)
+      @hash = @sample.save
+    end
+    
+    it "should be the first in the samples collection" do
+      @hash.should == @db.collection('samples').find_first
+    end
+    
+    it "should be saved in the db cache" do
+      cached = @db.collection(@sample.symbol_id).find_first
+      cached.should_not be_nil
+      cached['feature_vector'].should == @sample.feature_vector
+    end
+    
+  end
+  
+  describe "with some saved samples" do
+    
+    before(:each) do
+      @samples = (1..3).map { |i| Detexify::Sample.new('strokes' => @strokes, 'feature_vector' => [i], 'symbol_id' => @symbol.id) }
+      @hashes = @samples.map { |s| s.save }
+    end
+    
+    it "should save all to the db" do
+      @hashes.should == @db.collection('samples').find.to_a
+    end
+
+    it "should iterate over the last @limit added items" do
+      a = []
+      Detexify::Sample.each_stripped { |s| a << s }
+      a.map { |s| [s.symbol_id, s.feature_vector] }.should == @samples[-@limit..-1].map { |s| [s.symbol_id, s.feature_vector] }
+    end
+        
+    it "should not iterate over anything when unloaded" do
+      Detexify::Sample.unload
+      lambda { Detexify::Sample.each_stripped { raise 'Panic!' } }.should_not raise_error
+    end
+    
+    describe "and loaded fresh" do
+      before(:all) do
+        Detexify::Sample.unload
+        Detexify::Sample.load(true)
+      end
+      
+      it "should iterate over @limit items" do
+        num = 0
+        Detexify::Sample.each_stripped { |s| num += 1 }
+        num.should == @limit
+      end
+      
+      it "should iterate over the last @limit added items" do
+        a = []
+        Detexify::Sample.each_stripped { |s| a << s }
+        a.map { |s| [s.symbol_id, s.feature_vector] }.should == @samples[-@limit..-1].map { |s| [s.symbol_id, s.feature_vector] }
+      end
+      
+      after(:all) do
+        Detexify::Sample.unload
+      end
+    end
+    
+  end
+  
 end
 
-describe Detexify::MiniSampleContainer do
+describe Detexify::CappedContainer do
   
   before(:each) do
     @symbol = Latex::Symbol::List.first
     @strokes = [[{'x'=>1,'y'=>1}]]
-    @sample = Detexify::Sample.new :strokes => @strokes, :feature_vector => [1], :symbol_id => @symbol.id
-    @othersample = Detexify::Sample.new :strokes => @strokes, :feature_vector => [2], :symbol_id => @symbol.id
+    @sample = Detexify::Sample.new 'strokes' => @strokes, 'feature_vector' => [1], 'symbol_id' => @symbol.id
+    @othersample = Detexify::Sample.new 'strokes' => @strokes, 'feature_vector' => [2], 'symbol_id' => @symbol.id
     @limit = 1
-    @c = Detexify::MiniSampleContainer.new @limit
+    @c = Detexify::CappedContainer.new @limit
   end
   
-  it "can add a sample (as Minisample)" do
-    (@c << @sample).for_id(@sample.symbol_id).should === [Detexify::MiniSample.new(@sample)]
-  end
+  it "can add a sample" # do
+   #    (@c << @sample).for_id(@sample.symbol_id).should === [Detexify::MiniSample.new(@sample)]
+   #  end
   
-  it "should not add more that it's limit" do
-    (@c << @sample << @othersample).for_id(@sample.symbol_id).should have(@limit).samples
-  end
+  it "should not add more that it's limit" # do
+   #    (@c << @sample << @othersample).for_id(@sample.symbol_id).should have(@limit).samples
+   #  end
 
-  it "should contain the most recently added sample" do
-    (@c << @sample << @othersample).for_id(@sample.symbol_id).should include(@othersample)
-  end
+  it "should contain the most recently added sample" # do
+   #    (@c << @sample << @othersample).for_id(@sample.symbol_id).should include(@othersample)
+   #  end
 
-  it "should drop the oldest sample" do
-    (@c << @sample << @othersample).for_id(@sample.symbol_id).should_not include(@sample)
-  end
+  it "should drop the oldest sample" # do
+   #    (@c << @sample << @othersample).for_id(@sample.symbol_id).should_not include(@sample)
+   #  end
   
 end
